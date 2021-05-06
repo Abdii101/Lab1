d6f3f62bb7d3260e6f34fb0ca88d0829e7586239
Abdirahman Aden <>
2021-05-06 17:35:31 +0000

    a

---
+++ main.py
@@ -0,0 +1,37 @@
+# Question 1
+first_name = input('Input your First Name;') # Input your first name then press Enter
+last_name = input('Input your Last Name;') # Input your last name then press Enter 
+
+print("Hello  " + last_name +" "+ first_name)
+
+# Question 2
+
+number = int(input('Type a number_'))
+mod = number % 2 # mod ] wil define weather the number is even or odd or zero
+if mod > 0:
+    print('The number is Odd.')
+else:
+   print('The number is even.')
+if mod != 0:
+  print('The number is zero')
+ #  Question 3
+ 
+
+ #  Question 4
+x = 5 
+y = 5
+for w in range(0,x+1):
+  for k in range(y-w,0,-1):
+    print(k,end=' ')
+print()
+
+ #  Question 5
+def max_num()
+x = 5
+y = 5
+for w in range(0,x+1):
+  for k in range(y-w,0,-1):
+    print(k,end=' ')
+print()
+
+#
